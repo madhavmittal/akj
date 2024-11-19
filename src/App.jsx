77fe552ded1e7c53d4bf8042708e1468edd8f4ce
DarkMozart <contactmadhav.mittal@gmail.com>
2024-11-19 21:05:51 +0530

    Initial Commit- WhatsApp Button Only

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const sendWhatsAppMessage = () => {
+    const message = `ProducID and Customer Info`;
+    const url = `https://wa.me/919711579688?text=${encodeURIComponent(message)}`;
+    window.open(url, "_blank");
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={sendWhatsAppMessage}>
+          Contact on WhatsApp
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
